17c430d088cd2d8cb7d779813750094c20d1496d
LucasDotCampos <lucasdotcampos@gmail.com>
2022-03-15 17:51:48 -0300

    added/NadesEntity

---
+++ src/modules/nades/typeorm/entities/NadesEntity.ts
@@ -0,0 +1,28 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  Timestamp,
+  UpdateDateColumn,
+} from "typeorm";
+
+@Entity("nades")
+class NadesEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  picture: string;
+
+  @CreateDateColumn()
+  created_at: Timestamp;
+
+  @UpdateDateColumn()
+  updated_at: Timestamp;
+}
+
+export default NadesEntity;
