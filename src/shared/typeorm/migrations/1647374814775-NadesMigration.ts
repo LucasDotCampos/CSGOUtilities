e89dacb97678afad5c7cd41f8185a4585b81981e
LucasDotCampos <lucasdotcampos@gmail.com>
2022-03-15 17:35:21 -0300

    added/nadesmigration

---
+++ src/shared/typeorm/migrations/1647374814775-NadesMigration.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class NadesMigration1647374814775 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: "nades",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+            generationStrategy: "uuid",
+            default: "uuid_generate_v4()",
+          },
+          {
+            name: "name",
+            type: "varchar",
+          },
+          {
+            name: "picture",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+          {
+            name: "updated_at",
+            type: "timestamp with time zone",
+            default: "now()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("nades");
+    await queryRunner.query('DROP EXTENSION "uuid-ossp"');
+  }
+}
