ccb9b1043f72942b508fbfaf54b6591048624c9b
LucasDotCampos <lucasdotcampos@gmail.com>
2022-03-21 11:04:48 -0300

    added/user migration and entity

---
+++ src/modules/users/typeorm/entities/UserEntity.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  PrimaryGeneratedColumn,
+  Timestamp,
+  UpdateDateColumn,
+  OneToMany,
+} from "typeorm";
+import NadesEntity from "../../../nades/typeorm/entities/NadesEntity";
+
+@Entity("users")
+class UserEntity {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  avatar: string;
+
+  @OneToMany(() => NadesEntity, (user) => UserEntity)
+  nades: NadesEntity[];
+
+  @CreateDateColumn()
+  created_at: Timestamp;
+
+  @UpdateDateColumn()
+  updated_at: Timestamp;
+}
+
+export default UserEntity;
